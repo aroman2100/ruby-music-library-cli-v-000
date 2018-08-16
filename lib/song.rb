5da0d9bc11c10d2fc960d8834c9252f8cf0fa42d
Alex Roman <elpitbull6989@gmail.com>
2018-08-16 16:50:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
   extend Concerns::Findable
 
   attr_accessor :name, :artist, :genre
-  
+
 
   @@all = []
 
@@ -60,7 +60,7 @@ class Song
   end
 
   def self.new_from_filename(filename)
-      music = filename.split(" - ")
+      music = filename.chomp(".mp3").split(" - ")
       artist = Artist.find_or_create_by_name(music[0])
       genre = Genre.find_or_create_by_name(music[2].chomp(".mp3"))
       song = self.new(music[1],artist,genre)
